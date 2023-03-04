7f48a813f98263e89d3c5c7a0ccfd1ed5844dc9c
JanisZhang <janis.zhang@liferay.com>
2023-03-04 22:49:38 +0800

    Variables and Mutability

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,23 @@
-use std::{io, cmp::Ordering};
-use rand::Rng;
-
 fn main() {
 
-    println!("Guess the number");
+//Variables and Mutability: mutable and immutable
+    // let mut a = 5;
+    // println!("The value of a is: {a}");
+    // a = 6;
+    // println!("The value of a is: {a}");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // print!("{THREE_HOURS_IN_SECONDS}");
 
-    println!("The secret number is: {}", secret_number);
-
-    loop {
-        println!("Plz input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        // let guess: u32 = guess.trim().parse().expect("Pleas type a numebr!");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+//Shadowing is different from mut
+    let x = 5;
+    let x = x + 1;
+// Inner scope
+    {
+        let x = x * 2;
+        println!("Current value of x is: {x}");
     }
+
+    println!("The value of x is: {x}");
+
 }
