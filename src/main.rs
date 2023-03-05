0d033e03df59a93a44253cf81bf75ac6f179cf7c
JanisZhang <janis.zhang@liferay.com>
2023-03-05 11:38:41 +0800

    Add DataTypes

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,57 @@ fn main() {
     // print!("{THREE_HOURS_IN_SECONDS}");
 
 //Shadowing is different from mut
-    let x = 5;
-    let x = x + 1;
+    // let x = 5;
+    // let x = x + 1;
 // Inner scope
-    {
-        let x = x * 2;
-        println!("Current value of x is: {x}");
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("Current value of x is: {x}");
+    // }
 
-    println!("The value of x is: {x}");
+    // println!("The value of x is: {x}");
+
+//integer overflow
+        // let a: u8 = 256;
+        // println!("{}",a);
+
+//Numeric Operations
+        // //addition
+        // let sum = 5 + 10;
+        // println!("{}",sum);
+
+        // //Subreaction
+        // let difference = 95.5 - 4.3;
+        // println!("{}",difference);
+
+        // //multiplication
+        // let product = 4 * 30;
+        // println!("{}",product);
+
+        // //division
+        // let quptient = 56.7 / 32.2;
+        // println!("{}",quptient);
+
+        // let truncated1 = -10 / 3;
+        // println!("{}",truncated1);
+
+        // let truncated = -5 / 3;
+        // println!("{}",truncated);
+
+        // //remainder
+        // let remainder = 43 % 5;
+        // println!("{}",remainder);
+
+        // let t = true;
+        // let f = false;
+        // let c = 'z';
+        // print!("{c}");
+
+        let tuple = (500,6.4,1);
+        println!("{}",tuple.2);
+        // let (x,y,z) = tuple;
+        // println!("{}",x);  
+
+        
 
 }
